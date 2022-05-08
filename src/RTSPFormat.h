dc0bdd2d6303d2affde413964bcd1afb9f5b4ad3
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-05-08 19:59:01 +0200

    setter for period and size

--- src/RTSPFormat.h
+++ src/RTSPFormat.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "stdint.h"
 #include "platglue.h"
+#include "stdint.h"
 
 /**
  * @brief Class which provides the format information that will be retured for
@@ -10,13 +10,15 @@
 
 class RTSPFormat {
  public:
-  // Provides the format information: see see https://en.wikipedia.org/wiki/RTP_payload_formats
+  // Provides the format information: see see
+  // https://en.wikipedia.org/wiki/RTP_payload_formats
   virtual const char* format(char* buffer, int len) = 0;
-  // Potential data convertsion e.g. to network format. len is in samples, the result in bytes
-  virtual void convert(void* data, int len) { }
+  // Potential data convertsion e.g. to network format. len is in samples, the
+  // result in bytes
+  virtual void convert(void* data, int len) {}
   // Provides the fragment size in bytes
   virtual int fragmentSize() = 0;
-  virtual int timerPeriod() {return 20000;}
+  virtual int timerPeriod() { return 20000; }
 
  protected:
   const char* STD_URL_PRE_SUFFIX = "trackID";
@@ -67,22 +69,22 @@ class RTSPFormatPCM : public RTSPFormat {
 
   /**
    * @brief Provide format 10 or 11
-   * 
-   * @param buffer 
-   * @param len 
-   * @return const char* 
+   *
+   * @param buffer
+   * @param len
+   * @return const char*
    */
   const char* format(char* buffer, int len) override {
-    snprintf(
-        buffer, len,
-        "s=Microphone\r\n"      // Stream Name
-        "c=IN IP4 0.0.0.0\r\n"  // Connection Information
-        "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
-        "m=audio 0 RTP/AVP %d\r\n"  // UDP sessions with format 10 or 11
-        "a=rtpmap:%s\r\n"
-        "a=rate:%i\r\n",  // provide sample rate
-        format(channels()),payloadFormat(sampleRate(), channels()), sampleRate());
-    return (const char*) buffer;
+    snprintf(buffer, len,
+             "s=Microphone\r\n"      // Stream Name
+             "c=IN IP4 0.0.0.0\r\n"  // Connection Information
+             "t=0 0\r\n"  // start / stop - 0 -> unbounded and permanent session
+             "m=audio 0 RTP/AVP %d\r\n"  // UDP sessions with format 10 or 11
+             "a=rtpmap:%s\r\n"
+             "a=rate:%i\r\n",  // provide sample rate
+             format(channels()), payloadFormat(sampleRate(), channels()),
+             sampleRate());
+    return (const char*)buffer;
   }
 
   /**
@@ -94,25 +96,28 @@ class RTSPFormatPCM : public RTSPFormat {
   void convert(void* data, int samples) {
     // convert to network format (big endian)
     int16_t* pt_16 = (int16_t*)data;
-    for (int j = 0; j < samples/2; j++) {
+    for (int j = 0; j < samples / 2; j++) {
       pt_16[j] = htons(pt_16[j]);
     }
   }
 
+  PCMInfo& info() { return *p_info; }
 
   int sampleRate() { return p_info->getSampleRate(); }
   int channels() { return p_info->getChannels(); }
   int bytesPerSample() { return p_info->getSampleSizeBytes(); }
   // for sample rate 16000
   int fragmentSize() { return 320; }
-  int timerPeriod() {return 20000;}
+  int timerPeriod() { return 20000; }
 
-  PCMInfo& info() { return *p_info; }
-
+  void setFragmentSize(int fragmentSize) { fragment_size = fragmentSize; }
+  void setTimerPeriod(int period) { timer_period = period; }
 
  protected:
   PCMInfo* p_info = nullptr;
   char payload_fromat[30];
+  int fragment_size = 320;
+  int timer_period = 20000;
 
   const char* payloadFormat(int sampleRate, int channels) {
     // see https://en.wikipedia.org/wiki/RTP_payload_formats
@@ -120,12 +125,12 @@ class RTSPFormatPCM : public RTSPFormat {
 
     switch (channels) {
       case 1:
-        snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels), sampleRate,
-                 channels);
+        snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels),
+                 sampleRate, channels);
         break;
       case 2:
-        snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels), sampleRate,
-                 channels);
+        snprintf(payload_fromat, 30, "%d L16/%i/%i", format(channels),
+                 sampleRate, channels);
         break;
       default:
         log_e("unsupported audio type");
